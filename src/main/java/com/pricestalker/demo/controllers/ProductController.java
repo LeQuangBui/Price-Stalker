49bab58c9f6d5240510bf3ee59d3cc6874263872
Le Quang Bui <builquang2005@gmail.com>
2026-01-27 06:55:14 +0700

    Update Product Controller add Endpoint for search products and add products by url

--- src/main/java/com/pricestalker/demo/controllers/ProductController.java
+++ src/main/java/com/pricestalker/demo/controllers/ProductController.java
@@ -1,12 +1,17 @@
 package com.pricestalker.demo.controllers;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pricestalker.demo.entities.Product;
 import com.pricestalker.demo.services.ProductService;
@@ -16,6 +21,11 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
+	@PostMapping("/product")
+	public void addProduct(@RequestParam String url) throws IOException {
+		this.productService.addProduct(url);
+	}
+	
 	@GetMapping("/")
 	public String home(Model model) {
 		List<Product> products = this.productService.getAllProducts();
@@ -23,10 +33,25 @@ public class ProductController {
 		return "Products";
 	}
 	
+	
 	@GetMapping("/product/{id}")
 	public String getProduct(Model model, @PathVariable("id") String id) {
 		Product product = this.productService.getProduct(id);
 		model.addAttribute("product", product);
 		return "Product";
+	}
+	
+	@GetMapping("/product/search")
+	public ResponseEntity<ArrayList<String>> searchProducts(@RequestParam String searchText) {
+		List<Product> foundProducts = this.productService.getProductBySearchText(searchText);
+		ArrayList<String> names = new ArrayList<String>();
+		if (!foundProducts.isEmpty()) {
+			for (Product p: foundProducts) {
+				names.add(p.getName());
+			}
+			return ResponseEntity.ok(names);
+		} else {
+			return ResponseEntity.noContent().build();
+		}
 	}
 }
