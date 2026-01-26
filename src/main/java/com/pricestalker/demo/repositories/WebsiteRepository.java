85248a31606985ed67cf772fd353743475a65d24
Le Quang Bui <builquang2005@gmail.com>
2026-01-27 06:54:20 +0700

    Add logic to add Product by url

--- src/main/java/com/pricestalker/demo/repositories/WebsiteRepository.java
+++ src/main/java/com/pricestalker/demo/repositories/WebsiteRepository.java
@@ -1,9 +1,13 @@
 package com.pricestalker.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.pricestalker.demo.entities.Website;
 
 public interface WebsiteRepository extends JpaRepository<Website, String>{
-
+	@Query("SELECT w FROM Website w WHERE " +
+			"LOWER(:url) LIKE LOWER(CONCAT('%', w.url, '%'))")
+	Website findOneByUrl(@Param("url") String url);
 }
--- src/main/java/com/pricestalker/demo/services/ProductService.java
+++ src/main/java/com/pricestalker/demo/services/ProductService.java
@@ -1,20 +1,53 @@
 package com.pricestalker.demo.services;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.pricestalker.demo.entities.CssSelector;
 import com.pricestalker.demo.entities.Product;
+import com.pricestalker.demo.entities.Website;
 import com.pricestalker.demo.repositories.ProductRepository;
+import com.pricestalker.demo.repositories.WebsiteRepository;
 
 @Component
 public class ProductService {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired WebsiteRepository websiteRepository;
+	
 	public void addProduct(Product p) {
+		this.productRepository.save(p);
+	}
+	
+	public void addProduct(String url) throws IOException {
+		Website w = this.websiteRepository.findOneByUrl(url);
+		if (w == null) return;
+
+		Map<String, String> map = new HashMap<String, String>();
+        Document doc = Jsoup.connect(url).get();
+        
+        for (CssSelector s: w.getCssSelectors()) {
+        	Element e = doc.selectFirst(s.getSelectorString());
+        	map.put(s.getFieldName(), e.text().trim());
+        }
+        
+		Product p = new Product();
+		p.setName(map.get("name"));
+		p.setWebsite(w);
+		p.setUrl(url);
+		p.setCurrentPrice(Integer.parseInt(map.get("price_label").replaceAll("[^0-9]", "")));
+		p.setCurrency(map.get("price_label").replaceAll("[0-9., ]", ""));
+		
 		this.productRepository.save(p);
 	}
 	
@@ -27,6 +60,10 @@ public class ProductService {
 		Optional<Product> optional = this.productRepository.findById(id);
 		Product product = optional.get();
 		return product;
+	}
+	
+	public List<Product> getProductBySearchText(String searchText) {
+		return this.productRepository.findBySearchText(searchText);
 	}
 	
 	public void updateProduct(Product p, String id) {
